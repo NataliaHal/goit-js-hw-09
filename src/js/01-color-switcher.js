63df2ac2fa50f1e40d43fd0e4257a3fa34ed6a08
NataliaHal <118615900+NataliaHal@users.noreply.github.com>
2023-05-03 14:50:40 +0200

    “fix”

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+let intervalColor = null;
 
+buttonStop.setAttribute('disabled', '');
+
+buttonStart.addEventListener('click', onButtonStart);
+buttonStop.addEventListener('click', onButtonStop);
+
+function onButtonStart() {
+  onToggelBtnActivity();
+  changeColorBody();
+
+  intervalEl = setInterval(changeColorBody, 1000);
+}
+
+
+function onButtonStop() {
+  clearInterval(intervalEl);
+
+  onToggelBtnActivity();
+}
+
+function changeColorBody() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function onToggelBtnActivity() {
+  buttonStart.toggleAttribute('disabled');
+  buttonStop.toggleAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
