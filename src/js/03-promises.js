bca2e6f5c616976dccf87ae3e16bf479018d2295
NataliaHal <118615900+NataliaHal@users.noreply.github.com>
2023-05-04 21:27:11 +0200

    “homew”

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,44 +1,71 @@
-import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+
+const submitBtn = form.querySelector('button[type="submit"]');
+
+
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  submitBtn.setAttribute('disabled', '');
+
+ const {
+    elements: { delay, step, amount },
+  } = evt.currentTarget;
+
+  if (
+    formValidation(
+      Number(amount.value),
+      Number(step.value),
+      Number(delay.value)
+     )
+  ) {
+    submitBtn.removeAttribute('disabled');
+
+    return;
+  }
+
+  let currentDelay = Number(delay.value);
+
+  for (let i = 1; i <= amount.value; i += 1) {
+    createPromise(i, currentDelay)
+      .then(value => Notify.success(value))
+      .catch(error => Notify.failure(error))
+      .finally(() => {
+        if (Number(amount.value) === i) {
+          submitBtn.removeAttribute('disabled');
+        }
+      });
+
+    currentDelay += Number(step.value);
+  }
+});
+
+function formValidation(delay, step, amount) {
+  if (delay < 0 || step < 0 || amount < 0) {
+    Notify.failure('Negative numbers are invalid.');
+    return true;
+  }
+
+  if (!amount) {
+    Notify.failure('Create at least one promise.');
+    return true;
+  }
+
+  return false;
+}
 
 function createPromise(position, delay) {
-  return new Promise((resolve, reject) => {
-    const timerId = setTimeout(() => {
-      const shouldResolve = Math.random() > 0.3;
-      const result = { position, delay };
+  return new Promise((res, rej) => {
+    const shouldRes = Math.random() > 0.3;
 
-      if (shouldResolve) {
-        Notiflix.Notify.Success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-        resolve(result);
+    setTimeout(() => {
+      if (shouldRes) {
+        res(`✅ Fulfilled promise ${position} in ${delay}ms`);
       } else {
-        Notiflix.Notify.Failure(`❌ Rejected promise ${position} in ${delay}ms`);
-        reject(result);
+        rej(`❌ Rejected promise ${position} in ${delay}ms`);
       }
     }, delay);
-
-    // add the timerId to the result object
-    return { ...result, timerId };
   });
 }
 
-const form = document.querySelector('.form');
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  const formData = new FormData(event.target);
-  const delay = Number(formData.get('delay'));
-  const step = Number(formData.get('step'));
-  const amount = Number(formData.get('amount'));
-
-  // create an array of promises
-  const promises = Array.from({ length: amount }, (_, i) =>
-    createPromise(i + 1, delay + step * i)
-  );
-
-  Promise.all(promises)
-    .then(() => {
-      Notiflix.Notify.Success(`✅ All promises were fulfilled!`);
-    })
-    .catch(() => {
-      Notiflix.Notify.Failure(`❌ At least one promise was rejected.`);
-    });
-});
