447c9b68ea37c4ca91666101ae35c126b98a1aef
NataliaHal <118615900+NataliaHal@users.noreply.github.com>
2023-05-04 18:08:34 +0200

    “fix”

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,13 +1,13 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-import Notiflix from 'notiflix';
 
 const input = document.querySelector('#datetime-picker');
 const button = document.querySelector('[data-start]');
-const dataDays = document.querySelector('[data-days]');
-const dataHours = document.querySelector('[data-hours]');
-const dataMins = document.querySelector('[data-minutes]');
 const dataSecs = document.querySelector('[data-seconds]');
+const dataMins = document.querySelector('[data-minutes]');
+const dataHours = document.querySelector('[data-hours]');
+const dataDays = document.querySelector('[data-days]');
+
 let intervalId = null;
 
 button.setAttribute('disabled', 'disabled');
@@ -18,7 +18,6 @@ const onPress = () => {
     const time = currentDate - Date.now();
     const convertTime = convertMs(time);
     updateClockFace(convertTime);
-    // console.log(convertTime);
     if (time <= 1000) {
       clearInterval(intervalId);
     }
@@ -36,7 +35,6 @@ const options = {
   onClose(selectedDates) {
     if (selectedDates[0] < Date.now()) {
       Notiflix.Notify.failure('Please choose a date in the future');
-      //   alert('Please choose a date in the future');
       return;
     }
   },
@@ -50,32 +48,36 @@ const options = {
   },
 };
 
-flatpickr(input, options);
 
-button.addEventListener('click', onPress);
 
-//конвертує мс в дні:години:хвилини:секунди
-function convertMs(ms) {
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-  const days = Math.floor(ms / day);
-  const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  return { days, hours, minutes, seconds };
-}
-
-//додає 0 перед годинами/хвилинами/секундами
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
 
-// Передає значення таймеру в html
 function updateClockFace({ days, hours, minutes, seconds }) {
   dataDays.textContent = addLeadingZero(days);
   dataHours.textContent = addLeadingZero(hours);
   dataMins.textContent = addLeadingZero(minutes);
   dataSecs.textContent = addLeadingZero(seconds);
+}
+
+flatpickr(input, options);
+
+button.addEventListener('click', onPress);
+
+function convertMs(ms) {
+ 
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const hours = Math.floor((ms % day) / hour);
+  const days = Math.floor(ms / day); 
+  
+ 
+  
+ 
+  return { days, hours, minutes, seconds };
 }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      const result = { position, delay };
+      if (shouldResolve) {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        resolve(result);
+      } else {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        reject(result);
+      }
+    }, delay);
+  });
+}
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(event.currentTarget);
+  const delay = Number(formData.get('delay'));
+  const step = Number(formData.get('step'));
+  const amount = Number(formData.get('amount'));
+
+  for (let i = 1; i <= amount; i += 1) {
+    const position = i;
+    const promiseDelay = delay + step * (i - 1);
+    createPromise(position, promiseDelay);
   }
 }
